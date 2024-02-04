2e9a64f83c436cfc143f123bc3ff0322f3486eef
m.nechaev <m.nechaev@ritm.media>
2024-02-04 11:17:16 +0300

    Initial Commit

---
+++ 1_KickTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1_KickTask
+//
+//  Created by Нечаев Михаил on 04.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
